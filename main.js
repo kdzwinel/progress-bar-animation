2baeb63cbd515fac21b5c85d19f9a23fcec5b791
Konrad Dzwinel <kdzwinel@gmail.com>
2016-04-11 23:37:45 +0200

    CSS solution finished

--- main.js
+++ main.js
@@ -18,8 +18,101 @@ function getAvatar(idx) {
 
     return avatars[idx % avatars.length];
 }
+
+function conicGradient(from, to) {
+    let gradient = new ConicGradient({
+        stops: from + ", " + to,
+        size: 120
+    });
+    return gradient.png;
+}
 // HELPERS
 
-document.body.addEventListener('click', function() {
-    document.body.classList.toggle('rotate');
-});
+const style = document.createElement('style');
+style.type = 'text/css';
+document.getElementsByTagName('head')[0].appendChild(style);
+
+const gradientCache = new Map();
+function conicGradientClass(from, to) {
+    let className = gradientCache.get(from + to);
+
+    if (!className) {
+        const png = conicGradient(from, to);
+        className = `gradient-${gradientCache.size}`;
+
+        style.innerHTML += `.${className} { background-image: url('${png}'); }\n`;
+
+        gradientCache.set(from + to, className);
+    }
+
+    return className;
+}
+
+function changeAvatar(i) {
+    let player = avatar.animate([
+        {transform: 'rotateY(0) scale(1)'},
+        {transform: 'rotateY(90deg) scale(0.7)'}
+    ], {
+        easing: 'ease-in',
+        duration: 150
+    });
+
+    player.onfinish = () => {
+        document.querySelector('.avatar').style.backgroundImage = 'url(' + getAvatar(i) + ')';
+
+        avatar.animate([
+            {transform: 'rotateY(90deg) scale(0.7)'},
+            {transform: 'rotateY(0deg) scale(1)'}
+        ], {
+            easing: 'ease-out',
+            duration: 150
+        });
+    };
+
+}
+
+const doughnut = document.querySelector('.doughnut');
+const avatar = document.querySelector('.avatar');
+const circles = doughnut.querySelectorAll('.circle');
+const leftCircle = circles[0];
+const rightCircle = circles[1];
+let i = 0;
+
+function nextStep() {
+    i++;
+
+    changeAvatar(i);
+    leftCircle.className = 'circle ' + conicGradientClass(getColor(i), getColor(i + 1));
+    rightCircle.className = 'circle ' + conicGradientClass(getColor(i), getColor(i + 1));
+
+    let player = rightCircle.querySelector('.rotor').animate([
+        {
+            transform: 'rotate(0deg) translateZ(1px)'
+        },
+        {
+            transform: 'rotate(180deg) translateZ(1px)'
+        }
+    ], {
+        easing: 'ease-in',
+        fill: 'forwards',
+        duration: 750
+    });
+
+    player.onfinish = () => {
+        player = leftCircle.querySelector('.rotor').animate([
+            {
+                transform: 'rotate(-180deg)'
+            },
+            {
+                transform: 'rotate(0deg)'
+            }
+        ], {
+            easing: 'ease-out',
+            duration: 750
+        });
+
+        player.onfinish = nextStep;
+    };
+}
+
+nextStep();
